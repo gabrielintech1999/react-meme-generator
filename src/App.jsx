12ee17d5597b578787a92ae8d47f8f456cbd9657
Gabriel Gomes <gabrielgomesdeveloper1999@gmail.com>
2024-08-27 09:58:55 +0100

    basic features and stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [memes, setMemes] = useState([]);
+  const [meme, setMeme] = useState({
+    top: "",
+    bottom: "",
+    imgCover: "",
+  });
+
+
+  console.log(meme);
+  
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setMemes(data.data.memes));
+  }, []);
+
+  function getRandomMeme() {
+    const random = Math.floor(Math.random() * memes.length);
+
+    const newCoverImg = memes[random].url;
+
+    setMeme({
+      ...meme,
+      imgCover: newCoverImg,
+    });
+  }
+
+  function handleFormData(e) {
+    const { value, name } = e.target;
+
+    console.log(name);
+
+    setMeme({
+      ...meme,
+      [name]: value,
+    });
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <header>
+        <h2>Meme Generator</h2>
+
+        <p>React project</p>
+      </header>
+      <div className="wrapper">
+        <form>
+          <div className="input-container">
+            <input
+              onChange={handleFormData}
+              value={meme.top}
+              name="top"
+              type="text"
+              placeholder="top"
+              id=""
+            />
+            <input
+              onChange={handleFormData}
+              value={meme.bottom}
+              name="bottom"
+              type="text"
+              placeholder="bottom"
+              id=""
+            />
+          </div>
+          <button type="button" onClick={getRandomMeme}>
+            Get new meme
+          </button>
+        </form>
+        <main className="container" style={{  backgroundImage: `url(${meme.imgCover})`}}>
+          <div className="top">{meme.top}</div>
+          <div className="bottom">{meme.bottom}</div>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
